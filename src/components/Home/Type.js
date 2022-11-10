2bf50bf2558e2b76fc3993be5b50a7d8b40554e3
HondaCivico <84585853+Kyianthe1st@users.noreply.github.com>
2022-11-10 20:51:31 +0100

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,7 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Fast Learner"
         ],
         autoStart: true,
         loop: true,
